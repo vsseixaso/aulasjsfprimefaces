67cecca5f955d979e2fe4cb66075f69f67b7e250
viniciussso <viniciussso@CUSCUZ>
2017-09-15 11:31:59 -0300

    Estrutura inicial do projeto.

---
+++ aulasjsfprimefaces/src/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,13 @@
+package br.com.framework.implementacao.crud;
+
+/**
+ * 
+ * Nome do caminho JNDI datasource Tomcat
+ * @author viniciussso
+ *
+ */
+public class VariavelConexaoUtil {
+
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+	
+}
